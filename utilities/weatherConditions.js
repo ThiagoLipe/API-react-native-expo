c1498c902f9df614ff6caeacc2f2242d014c7422
Thiago Felipe <108498586+ThiagoLipe@users.noreply.github.com>
2023-06-16 18:10:22 -0300

    Add files via upload

---
+++ utilities/weatherConditions.js
@@ -0,0 +1,51 @@
+export const weatherConditions = {
+  Rain: {
+    color: '#005BEA',
+    title: 'Chovendo',
+    subtitle: 'Pegue um copo de café',
+    icon: 'weather-rainy'
+  },
+  Clear: {
+    color: '#f7f033',
+    title: 'Ensolarado',
+    subtitle: 'Perfeito para usar óculos de sol!',
+    icon: 'weather-sunny'
+  },
+  Thunderstorm: {
+    color: '#525252',
+    title: 'Tempestade próxima',
+    subtitle: 'Melhor ficar em casa!',
+    icon: 'weather-lightning'
+  },
+  Clouds: {
+    color: '#403861',
+    title: 'Nublado',
+    subtitle: 'Nem tão quente, nem tão frio!',
+    icon: 'weather-cloudy'
+  },
+
+  Snow: {
+    color: '#00d2ff',
+    title: 'Neve',
+    subtitle: 'Perfeito para fazer um boneco de neve!',
+    icon: 'weather-snowy'
+  },
+  Drizzle: {
+    color: '#076585',
+    title: 'Choviscando',
+    subtitle: 'Parcialmente chovendo...',
+    icon: 'weather-hail'
+  },
+  Haze: {
+    color: '#66A6FF',
+    title: 'Neblina',
+    subtitle: 'Dobre a sua atenção!',
+    icon: 'weather-hail'
+  },
+  Mist: {
+    color: '#3CD3AD',
+    title: 'Nevoeiro',
+    subtitle: "Triplique sua atenção!",
+    icon: 'weather-fog'
+  }
+};
